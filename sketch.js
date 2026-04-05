b123b0e9e094d2c7ee9c60641a4bca09f8e2b5a1
gasoide <grandgreif@gmail.com>
2026-04-05 18:59:40 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let t = 0;
+let intensity = 1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -7,14 +8,14 @@ function setup() {
 }
 
 function draw() {
-  background(0, 0, 0, 10); // scia morbida
+  background(0, 0, 0, 10);
 
   translate(width / 2, height / 2);
 
-  let numBlobs = 40;
-  for (let i = 0; i < numBlobs; i++) {
-    let angle = (TWO_PI / numBlobs) * i + t * 0.3;
-    let radius = 150 + 80 * sin(t + i * 0.3);
+  let num = 40;
+  for (let i = 0; i < num; i++) {
+    let angle = (TWO_PI / num) * i + t * 0.3;
+    let radius = 150 + 80 * sin(t + i * 0.3) * intensity;
 
     let x = radius * cos(angle);
     let y = radius * sin(angle);
@@ -22,37 +23,23 @@ function draw() {
     let hue = (t * 40 + i * 10) % 360;
     fill(hue, 80, 100, 70);
 
-    let r = 40 + 20 * sin(t * 2 + i);
+    let r = 40 + 20 * sin(t * 2 + i) * intensity;
     ellipse(x, y, r, r);
-saveFrameIfNeeded();
   }
 
   t += 0.01;
 }
 
+function mouseMoved() {
+  intensity = map(mouseX, 0, width, 0.2, 3);
+}
+
+function keyPressed() {
+  if (key === 'c') {
+    background(0);
+  }
+}
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
-
-let recording = false;
-let frameId = 0;
-
-function keyPressed() {
-  if (key === 'r') {
-    recording = !recording;
-    if (recording) {
-      frameId = 0;
-      console.log("REC ON");
-    } else {
-      console.log("REC OFF");
-    }
-  }
-}
-
-function saveFrameIfNeeded() {
-  if (recording) {
-    saveCanvas('frame_' + nf(frameId, 5), 'png');
-    frameId++;
-  }
-}
-
